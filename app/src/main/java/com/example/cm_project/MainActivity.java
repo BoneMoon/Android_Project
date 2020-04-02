9fb358f9a14266a45fc81a58c5b665038a111d7e
BoneMoon <5josediogo1@gmail.com>
2020-04-02 14:46:46 +0100

    Merge into master

--- app/src/main/java/com/example/cm_project/MainActivity.java
+++ app/src/main/java/com/example/cm_project/MainActivity.java
@@ -13,6 +13,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
     //
+    //
+    
     public void btnNotas(View v) {
         Intent i = new Intent(MainActivity.this, NotaActivity.class);
         startActivity(i);
